5d4eb35e4d44cee0c2dafe91ca97c3aa0cf3bcc6
krozzus <50258863+krozzus@users.noreply.github.com>
2021-04-08 12:32:07 +0200

    regex change

--- jira-content.js
+++ jira-content.js
@@ -3,9 +3,9 @@
         const issueLinkElements = document.querySelectorAll(".aui-page-header-main a.issue-link");
         const issueTitleElement = document.querySelector("h1#summary-val");
         const issueType = document.querySelector("#type-val").innerText.trim();
-		const issueFixVersion = document.querySelector("#fixfor-val").innerText.trim();
-		const issuePriority = document.querySelector("#priority-val").innerText.trim();
-		let dropdownOpen = false;
+        const issueFixVersion = document.querySelector("#fixfor-val").innerText.trim();
+        const issuePriority = document.querySelector("#priority-val").innerText.trim();
+        let dropdownOpen = false;
         
         if (!issueLinkElements || !issueLinkElements.length || !issueTitleElement) return;
         
@@ -31,7 +31,7 @@
         }
 
         function replaceBadSymbols(str) {
-            return str.replace(/[^\w_\.\(\)\s]+/g, ' ');
+            return str.replaceAll(/[^\w\.\\\/\s-]+/g, '_');
         }
 
         function createClipboardButton(iconClass, title, clickFn) {
@@ -48,11 +48,11 @@
             let dropdownElement = document.createElement("span");
             dropdownElement.style.cursor = "pointer";
             dropdownElement.style.marginLeft = "10px";
-			dropdownElement.style.border = "thin solid #000000";
-			dropdownElement.style.borderRadius = "25px";
-			dropdownElement.style.padding = "5px 5px 5px 5px"; 
-			dropdownElement.innerText = innertext;
-			dropdownElement.id = id;
+            dropdownElement.style.border = "thin solid #000000";
+            dropdownElement.style.borderRadius = "25px";
+            dropdownElement.style.padding = "5px 5px 5px 5px"; 
+            dropdownElement.innerText = innertext;
+            dropdownElement.id = id;
             dropdownElement.onclick = clickFn;
             return dropdownElement;
         }
@@ -70,47 +70,47 @@
         
         containerElement.appendChild(createClipboardButton("aui-iconfont-branch", "Copy branch name to clipboard", function() {
             const issueGitName = (replaceBadSymbols(issueDescription)).split(' ').join('-');
-			if(issueType == "Bug" && (issuePriority == "Critical" || issuePriority == "Blocker")) {
-				if(issueFixVersion != "None"){
-					const issueTypeGitName = `hotfix/${issueGitName}`;
-					navigator.clipboard.writeText(issueTypeGitName);
-				}
-				else{
-					const issueTypeGitName = `bugfix/${issueGitName}`;
-					navigator.clipboard.writeText(issueTypeGitName);
-				}
-			}
-			else {
-				const issueTypeGitName = `feature/${issueGitName}`;
-				navigator.clipboard.writeText(issueTypeGitName);
-			}
+            if(issueType == "Bug" && (issuePriority == "Critical" || issuePriority == "Blocker")) {
+                if(issueFixVersion != "None"){
+                    const issueTypeGitName = `hotfix/${issueGitName}`;
+                    navigator.clipboard.writeText(issueTypeGitName);
+                }
+                else{
+                    const issueTypeGitName = `bugfix/${issueGitName}`;
+                    navigator.clipboard.writeText(issueTypeGitName);
+                }
+            }
+            else {
+                const issueTypeGitName = `feature/${issueGitName}`;
+                navigator.clipboard.writeText(issueTypeGitName);
+            }
         }));
         
-   		containerElement.appendChild(createClipboardButton("aui-iconfont-arrow-down-small", "Dropdown to choose prefix for the branch", function() {
-			if(!dropdownOpen){
-			dropdownOpen = true;
-			this.classList.replace("aui-iconfont-arrow-down-small","aui-iconfont-chevron-left");
-			const issueGitName = (replaceBadSymbols(issueDescription)).split(' ').join('-');
-			containerElement.appendChild(createDropdownOption("bugfixButton", "bugfix", function() {
-				let issueTypeGitName = `bugfix/${issueGitName}`;
-				navigator.clipboard.writeText(issueTypeGitName);
-			}));
-			containerElement.appendChild(createDropdownOption("hotfixButton", "hotfix", function() {
-				let issueTypeGitName = `hotfix/${issueGitName}`;
-				navigator.clipboard.writeText(issueTypeGitName);
-			}));
-			containerElement.appendChild(createDropdownOption("featureButton", "feature", function() {
-				let issueTypeGitName = `feature/${issueGitName}`;
-				navigator.clipboard.writeText(issueTypeGitName);
-			}));
-			}
-			else{
-				dropdownOpen = false;
-				containerElement.removeChild(containerElement.childNodes[4]);
-				containerElement.removeChild(containerElement.childNodes[4]);
-				containerElement.removeChild(containerElement.childNodes[4]);
-				this.classList.replace("aui-iconfont-chevron-left","aui-iconfont-arrow-down-small");
-			}
+        containerElement.appendChild(createClipboardButton("aui-iconfont-arrow-down-small", "Dropdown to choose prefix for the branch", function() {
+            if(!dropdownOpen){
+                dropdownOpen = true;
+                this.classList.replace("aui-iconfont-arrow-down-small","aui-iconfont-chevron-left");
+                const issueGitName = (replaceBadSymbols(issueDescription)).split(' ').join('-');
+                containerElement.appendChild(createDropdownOption("bugfixButton", "bugfix", function() {
+                    let issueTypeGitName = `bugfix/${issueGitName}`;
+                    navigator.clipboard.writeText(issueTypeGitName);
+                }));
+                containerElement.appendChild(createDropdownOption("hotfixButton", "hotfix", function() {
+                    let issueTypeGitName = `hotfix/${issueGitName}`;
+                    navigator.clipboard.writeText(issueTypeGitName);
+                }));
+                containerElement.appendChild(createDropdownOption("featureButton", "feature", function() {
+                    let issueTypeGitName = `feature/${issueGitName}`;
+                    navigator.clipboard.writeText(issueTypeGitName);
+                }));
+            }
+            else{
+                dropdownOpen = false;
+                containerElement.removeChild(containerElement.childNodes[4]);
+                containerElement.removeChild(containerElement.childNodes[4]);
+                containerElement.removeChild(containerElement.childNodes[4]);
+                this.classList.replace("aui-iconfont-chevron-left","aui-iconfont-arrow-down-small");
+            }
         }));
     }
 
